8264adde9a024d3b94a05fc0c8c98bcfa66b4e44
n0daft <eric.neher@gmx.ch>
2015-03-05 18:52:40 +0100

    new voucher menu adjusted

--- VTrack/app/src/main/java/ch/mobop/mse/vtrack/NewVoucherActivity.java
+++ VTrack/app/src/main/java/ch/mobop/mse/vtrack/NewVoucherActivity.java
@@ -3,7 +3,10 @@ package ch.mobop.mse.vtrack;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,16 +17,46 @@ import ch.mobop.mse.vtrack.widgets.DatePickerFragment;
  */
 public class NewVoucherActivity extends FragmentActivity  implements DatePickerFragment.EditDateDialogListener {
 
+    private EditText txtVoucherName;
     private TextView lblReceivedAt;
     private TextView lblValidUntil;
+    private EditText txtReceivedFrom;
+    private EditText txtNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_voucher);
 
-        lblReceivedAt = (TextView) findViewById(R.id.txtReceivedAt);
-        lblValidUntil = (TextView) findViewById(R.id.txtValidUntil);
+        // Get UI component references
+        txtVoucherName = (EditText) findViewById(R.id.txtVoucherName);
+        lblReceivedAt = (TextView) findViewById(R.id.lblReceivedAt);
+        lblValidUntil = (TextView) findViewById(R.id.lblValidUntil);
+        txtReceivedFrom = (EditText) findViewById(R.id.txtReceivedFrom);
+        txtNotes = (EditText) findViewById(R.id.txtNotes);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.new_voucher, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.action_new_voucher_cancel:
+                finish();
+
+            case R.id.action_new_voucher_save:
+                //TODO read inputs and create voucher entity.
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void handleReceivedAt(View v){
