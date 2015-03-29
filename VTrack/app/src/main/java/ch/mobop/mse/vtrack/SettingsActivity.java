2127cbdf05ef4f4b14fa3d5816340809dbcbb670
n0daft <eric.neher@gmx.ch>
2015-03-29 16:16:02 +0200

    app localized

---
+++ VTrack/app/src/main/java/ch/mobop/mse/vtrack/SettingsActivity.java
@@ -0,0 +1,20 @@
+package ch.mobop.mse.vtrack;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by n0daft on 28.03.2015.
+ */
+public class SettingsActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
+
+
+
+}
